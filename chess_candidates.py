117b43f3e9df4e93932a971fc306f5eac849b66a
Kerem Cömert <kerem0comert@gmail.com>
2022-06-19 17:43:58 +0200

    Create dataclass for player

--- chess_candidates.py
+++ chess_candidates.py
@@ -11,28 +11,35 @@ from dataclasses import dataclass
 
 DRIVER_PATH = "./chromedriver.exe"
 URL = "https://chess24.com/en/watch/live-tournaments/fide-candidates-2022/3/1/"
-EVALUATOR_SELECTOR = "evalindicatorTooltip"
-REGEX_FOR_EVALUATOR_SCORE = 'data-tooltip="([^"]*)\|'
-
+EVALUATOR_CLASS = "evalindicatorTooltip"
+EVALUATOR_REGEX = 'data-tooltip="([^"]*)\|'
+RUNNING_TIME_CLASS = "runningTime black"
+RUNNING_TIME_REGEX = 'runningTime black="([^"]*)\"'
+TIME_PRESSURE_THRESHOLD = 10
 
 @dataclass
+class Player:
+    name: str
+    is_time_pressured: bool = False
+@dataclass
 class Match:
-    match_id: 1
-    players: str
-    evaluation: float
+    match_id: int
+    white: Player
+    black: Player
+    evaluation: float = 0
 
+    def get_players(self) -> str: return f"{self.white.name}-{self.black.name}"
 
 def send_telegram_msg(msg: str, match: Match) -> None:
-    to_send: str = f"{datetime.now().time()}-{match.players}: {msg}"
+    to_send: str = f"{datetime.now().time()}-{match.get_players()}: {msg}"
     print(to_send)
     telegram_send.send(messages=[to_send])
 
-
 matches: list[Match] = [
-    Match(1, "Ding-Rapport", 0),
-    Match(2, "Caruana-Duda", 0),
-    Match(3, "Radjabov-Nepomniachtchi", 0),
-    Match(4, "Firouzja-Nakamura", 0),
+    Match(1, Player("Ding"), Player("Rapport")),
+    Match(2, Player("Caruana"), Player("Duda")),
+    Match(3, Player("Radjabov"), Player("Nepomniachtchi")),
+    Match(4, Player("Firouzja"), Player("Nakamura")),
 ]
 
 driver: webdriver.Chrome = webdriver.Chrome(DRIVER_PATH)
@@ -42,18 +49,35 @@ while 1:
         driver.get(f"{URL}{match.match_id}")
         evaluator_html: str = (
             WebDriverWait(driver, 10)
-            .until(EC.visibility_of_element_located((By.CLASS_NAME, EVALUATOR_SELECTOR)))
+            .until(
+                EC.visibility_of_element_located((By.CLASS_NAME, EVALUATOR_CLASS))
+            )
             .get_attribute("innerHTML")
         )
+        '''
+        running_time_html: str = (
+            WebDriverWait(driver, 10)
+            .until(
+                EC.visibility_of_element_located((By.CLASS_NAME, RUNNING_TIME_CLASS))
+            )
+            .get_attribute("innerHTML")
+        )
+        '''
         try:
             evaluation: float = float(
-                re.findall(REGEX_FOR_EVALUATOR_SCORE, evaluator_html)[0]
+                re.findall(EVALUATOR_REGEX, evaluator_html)[0]
+            )
+            
+            print(
+                f"For {match.get_players()}, current evaluation: "
+                f"{evaluation} previous evaluation: {match.evaluation}"
             )
             if evaluation != match.evaluation:
                 send_telegram_msg(
                     f"Evaluation moved from {match.evaluation} to {evaluation}", match
                 )
                 match.evaluation = evaluation
+            sleep(1)
         except IndexError:
             send_telegram_msg(
                 "Evaluation value could not be found with the regex.", match
